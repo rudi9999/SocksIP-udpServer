5b4b970aaabe426d175d4a3acac44b84f4c84c6c
rudi9999 <67137156+rudi9999@users.noreply.github.com>
2022-12-16 23:35:23 -0300

    @Rufu99

--- UDPserver.sh
+++ UDPserver.sh
@@ -613,7 +613,7 @@ After=network.target
 Type=simple
 User=root
 WorkingDirectory=/root
-ExecStart=/usr/bin/udpServer -ip=$ip_publica -net=$interfas -mode=system
+ExecStart=/usr/bin/udpServer -ip=$ip_publica -net=$interfas$Port -mode=system
 Restart=always
 RestartSec=3s
 
@@ -631,8 +631,45 @@ EOF
 	fi
 }
 
+exclude(){
+  title 'Excluir puertos UDP'
+  print_center -ama 'UDPserver cubre el rango total de puertos.'
+  print_center -ama 'puedes excluir puertos UDP'
+  msg -bar3
+  print_center -ama 'Ejemplos de puertos a excluir:'
+  print_center -ama 'dnstt (slowdns) udp 53 5300'
+  print_center -ama 'wireguard udp 51820'
+  print_center -ama 'openvpn udp 1194'
+  msg -bar
+  print_center -verd 'ingresa los puertos separados por espacios'
+  print_center -verd 'Ejemplo: 53 5300 51820 1194'
+  msg -bar3
+  in_opcion_down 'digita puertos o enter saltar'
+  del 2
+  tmport=($opcion)
+  for (( i = 0; i < ${#tmport[@]}; i++ )); do
+    num=$((${tmport[$i]}))
+    if [[ $num -gt 0 ]]; then
+      echo "$(msg -ama " Puerto a excluir >") $(msg -azu "$num") $(msg -verd "OK")"
+      Port+=" $num"
+    else
+      msg -verm2 " No es un puerto > ${tmport[$i]}?"
+      continue
+    fi
+  done
+
+  if [[ -z $Port ]]; then
+    unset Port
+    print_center -ama 'no se excluyeron puertos'
+  else
+    Port=" -exclude=$(echo "$Port"|sed "s/ /,/g"|sed 's/,//')"
+  fi
+  mag -bar3
+}
+
 install_UDP(){
 	title 'INSTALACION UDPserver'
+  exclude
 	download_udpServer
 	if [[ $(type -p udpServer) ]]; then
 		make_service
@@ -695,11 +732,115 @@ QUIC_SCRIPT(){
 	time_reboot 10
 }
 
+add_exclude(){
+  title 'Excluir puertos UDP'
+  print_center -ama 'UDPserver cubre el rango total de puertos.'
+  print_center -ama 'puedes excluir puertos UDP'
+  msg -bar3
+  print_center -ama 'Ejemplos de puertos a excluir:'
+  print_center -ama 'dnstt (slowdns) udp 53 5300'
+  print_center -ama 'wireguard udp 51820'
+  print_center -ama 'openvpn udp 1194'
+  msg -bar
+  print_center -verd 'ingresa los puertos separados por espacios'
+  print_center -verd 'Ejemplo: 53 5300 51820 1194'
+  in_opcion_down 'Ingresa puertos o enter para canselar'
+  del 4
+  tmport=($opcion)
+  unset Port
+  for (( i = 0; i < ${#tmport[@]}; i++ )); do
+    num=$((${tmport[$i]}))
+    if [[ $num -gt 0 ]]; then
+      echo "$(msg -ama " Puerto a excluir >") $(msg -azu "$num") $(msg -verd "OK")"
+      Port+=" $num"
+    else
+      msg -verm2 " No es un puerto > ${tmport[$i]}?"
+      continue
+    fi
+  done
+  if [[ $Port = "" ]]; then
+    unset Port
+    print_center -ama 'no se excluyeron puertos'
+  else
+    exclude=$(cat /etc/systemd/system/UDPserver.service|grep 'exclude')
+    if systemctl is-active UDPserver &>/dev/null; then
+      systemctl stop UDPserver &>/dev/null
+      systemctl disable UDPserver &>/dev/null
+      iniciar=1
+    fi
+    if [[ -z $exclude ]]; then
+      Port=" -exclude=$(echo "$Port"|sed "s/ /,/g"|sed 's/,//')"
+      sed -i "s/ -mode/$Port -mode/" /etc/systemd/system/UDPserver.service
+    else
+      exclude_port=$(echo $exclude|awk '{print $4}'|cut -d '=' -f2)
+      Port="-exclude=$exclude_port$(echo "$Port"|sed "s/ /,/g")"
+      sed -i "s/-exclude=$exclude_port/$Port/" /etc/systemd/system/UDPserver.service
+    fi
+    if [[ $iniciar = 1 ]]; then
+      systemctl start UDPserver &>/dev/null
+      systemctl enable UDPserver &>/dev/null
+    fi
+  fi
+  enter
+}
+
+quit_exclude(){
+  title 'QUITAR PUERTO DE EXCLUCION'
+  exclude=$(cat /etc/systemd/system/UDPserver.service|grep 'exclude'|awk '{print $4}')
+  ports=($port)
+  for (( i = 0; i < ${#ports[@]}; i++ )); do
+    a=$(($i+1))
+    echo "             $(msg -verd "[$a]") $(msg -verm2 '>') $(msg -azu "${ports[$i]}")"
+  done
+  if [[ ! ${#ports[@]} = 1 ]]; then
+    let a++
+    msg -bar
+    echo "             $(msg -verd "[0]") $(msg -verm2 ">") $(msg -bra "\033[1;41mVOLVER")  $(msg -verd "[$a]") $(msg -verm2 '> QUITAR TODOS')"
+    msg -bar
+  else
+    msg -bar
+    echo "             $(msg -verd "[0]") $(msg -verm2 ">") $(msg -bra "\033[1;41mVOLVER")"
+    msg -bar
+  fi
+  opcion=$(selection_fun $a)
+  [[ $opcion = 0 ]] && return
+  if systemctl is-active UDPserver &>/dev/null; then
+    systemctl stop UDPserver &>/dev/null
+    systemctl disable UDPserver &>/dev/null
+    iniciar=1
+  fi
+  if [[ $opcion = $a ]]; then
+    sed -i "s/$exclude //" /etc/systemd/system/UDPserver.service
+    print_center -ama 'Se quito todos los puertos excluidos'
+  else
+    let opcion--
+    unset Port
+    for (( i = 0; i < ${#ports[@]}; i++ )); do
+      [[ $i = $opcion ]] && continue
+      echo "$(msg -ama " Puerto a excluir >") $(msg -azu "${ports[$i]}") $(msg -verd "OK")"
+      Port+=" ${ports[$i]}"
+    done
+    Port=$(echo $Port|sed 's/ /,/g')
+    sed -i "s/$exclude/-exclude=$Port/" /etc/systemd/system/UDPserver.service
+  fi
+  if [[ $iniciar = 1 ]]; then
+    systemctl start UDPserver &>/dev/null
+    systemctl enable UDPserver &>/dev/null
+  fi
+  enter
+}
+
 menu_udp(){
 	title 'SCRIPT DE CONFIGRACION UDPserver BY @Rufu99'
 	print_center -ama 'UDPserver Binary by team newtoolsworks'
 	print_center -ama 'UDPclient Android SocksIP'
 	msg -bar
+  port=$(cat /etc/systemd/system/UDPserver.service|grep 'exclude')
+  if [[ ! $port = "" ]]; then
+    port=$(echo $port|awk '{print $4}'|cut -d '=' -f2|sed 's/,/ /g')
+    print_center -ama "PUERTOS EXCLUIDOS $port"
+    msg -bar
+  fi
 	ram=$(printf '%-8s' "$(free -m | awk 'NR==2{printf "%.2f%%", $3*100/$2 }')")
 	cpu=$(printf '%-1s' "$(top -bn1 | awk '/Cpu/ { cpu = "" 100 - $8 "%" }; END { print cpu }')")
 	echo " $(msg -verd 'IP:') $(msg -azu "$ip_publica")  $(msg -verd 'Ram:') $(msg -azu "$ram") $(msg -verd 'CPU:') $(msg -azu "$cpu")"
@@ -712,16 +853,24 @@ menu_udp(){
 		fi
 		echo " $(msg -verd "[1]") $(msg -verm2 '>') $(msg -verm2 'DESINSTALAR UDPserver')"
 		echo -e " $(msg -verd "[2]") $(msg -verm2 '>') $(msg -azu 'INICIAR/DETENER UDPserver') $estado"
+    echo " $(msg -verd "[3]") $(msg -verm2 '>') $(msg -azu 'REOMOVER SCRIPT')"
 		msg -bar3
-		echo " $(msg -verd "[3]") $(msg -verm2 '>') $(msg -verd 'CREAR CLIENTE')"
-		echo " $(msg -verd "[4]") $(msg -verm2 '>') $(msg -verm2 'REMOVER CLIENTE')"
-		echo " $(msg -verd "[5]") $(msg -verm2 '>') $(msg -ama 'RENOVAR CLIENTE')"
-		echo " $(msg -verd "[6]") $(msg -verm2 '>') $(msg -azu 'BLOQUEAR/DESBLOQUEAR CLIENTE')"
-		echo " $(msg -verd "[7]") $(msg -verm2 '>') $(msg -blu 'DETELLES DE LOS CLIENTES')"
-		echo " $(msg -verd "[8]") $(msg -verm2 '>') $(msg -azu 'LIMITADO DE CUENTAS')"
+		echo " $(msg -verd "[4]") $(msg -verm2 '>') $(msg -verd 'CREAR CLIENTE')"
+		echo " $(msg -verd "[5]") $(msg -verm2 '>') $(msg -verm2 'REMOVER CLIENTE')"
+		echo " $(msg -verd "[6]") $(msg -verm2 '>') $(msg -ama 'RENOVAR CLIENTE')"
+		echo " $(msg -verd "[7]") $(msg -verm2 '>') $(msg -azu 'BLOQUEAR/DESBLOQUEAR CLIENTE')"
+		echo " $(msg -verd "[8]") $(msg -verm2 '>') $(msg -blu 'DETELLES DE LOS CLIENTES')"
+		echo " $(msg -verd "[9]") $(msg -verm2 '>') $(msg -azu 'LIMITADO DE CUENTAS')"
 		msg -bar3
-		echo " $(msg -verd "[9]") $(msg -verm2 '>') $(msg -azu 'REOMOVER SCRIPT')"
-		num=9; a=x; b=1
+    print_center -ama 'EXCLUCION DE PUERTO'
+    msg -bar3
+    echo " $(msg -verd "[10]") $(msg -verm2 '>') $(msg -verd 'AGREGAR PUERTO A LISTA DE EXCLUSION')"
+		num=10
+    if [[ ! $port = "" ]]; then
+      echo " $(msg -verd "[11]") $(msg -verm2 '>') $(msg -verm2 'QUITAR PUERTO A LISTA DE EXCLUSION')"
+      num=11
+    fi
+    a=x; b=1
 	else
 		echo " $(msg -verd "[1]") $(msg -verm2 '>') $(msg -verd 'INSTALAR UDPserver')"
 		num=1; a=1; b=x
@@ -733,13 +882,15 @@ menu_udp(){
 		$a)install_UDP;;
 		$b)uninstall_UDP;;
 		2)reset;;
-		3)new_user;;
-		4)remove_user;;
-		5)renew_user;;
-		6)block_user;;
-		7)detail_user;;
-		8)limiter;;
-		9)QUIC_SCRIPT;;
+    3)QUIC_SCRIPT;;
+		4)new_user;;
+		5)remove_user;;
+		6)renew_user;;
+		7)block_user;;
+		8)detail_user;;
+		9)limiter;;
+    10)add_exclude;;
+    11)quit_exclude;;
 		0)return 1;;
 	esac
 }
